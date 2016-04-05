9bb1bad742f2f6e78fa4d07e2e886794498d89b4
gaa7par <gaa7par@gmail.com>
2016-04-05 21:59:13 +0200

    house_password.py added (tests failed)

---
+++ house_password.py
@@ -0,0 +1,21 @@
+def checkio(data):
+    number, upper, lower = False
+    if len(data) < 10:
+        return False
+    else:
+        for char in data:
+            number = char.isnumeric() or False
+            upper = char.isupper() or False
+            lower = char.lower() or False
+
+        return number and upper and lower
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio('A1213pokl') == False, "1st example"
+    assert checkio('bAse730onE4') == True, "2nd example"
+    assert checkio('asasasasasasasaas') == False, "3rd example"
+    assert checkio('QWERTYqwerty') == False, "4th example"
+    assert checkio('123456123456') == False, "5th example"
+    assert checkio('QwErTy911poqqqq') == True, "6th example"
+
