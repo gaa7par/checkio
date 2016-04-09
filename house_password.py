ebe5835565c8478b79a9d72b440208846203d2ad
gaa7par <gaa7par@gmail.com>
2016-04-09 22:30:53 +0200

    house_password.py solved

--- house_password.py
+++ house_password.py
@@ -16,12 +16,3 @@ def checkio(data):
                 lower = True
     if numeric and upper and lower:
         return True
-
-if __name__ == '__main__':
-    assert checkio('A1213pokl') == False, "1st example"
-    assert checkio('bAse730onE4') == True, "2nd example"
-    assert checkio('asasasasasasasaas') == False, "3rd example"
-    assert checkio('QWERTYqwerty') == False, "4th example"
-    assert checkio('123456123456') == False, "5th example"
-    assert checkio('QwErTy911poqqqq') == True, "6th example"
-
