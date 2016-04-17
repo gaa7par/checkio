d9e17be9190bedfe2c68feef17a3e150b3292bb1
gaa7par <gaa7par@gmail.com>
2016-04-17 21:47:29 +0200

    median.py refactored

--- median.py
+++ median.py
@@ -1,12 +1,11 @@
 # module statistics (with median()) not allowed
 def checkio(data):
-    median_minus = len(data) // 2 - 1
-    median_plus = len(data) // 2
+    median = len(data) // 2
 
     data.sort()
     if len(data) % 2 != 0:
-        median = data[len(data) // 2]
+        median = data[median]
     else:
-        median = (data[median_minus] + data[median_plus]) / 2
+        median = (data[median] + data[median - 1]) / 2
     return median
 
